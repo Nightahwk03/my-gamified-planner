f127b928037d3011f711b57b15884d80b7d2e81d
Nightahwk03 <kavishdoshi133@gmail.com>
2026-06-24 18:16:59 +0530

    Add files via upload

--- berries.js
+++ berries.js
@@ -2,7 +2,7 @@ const pokemonBerries = [
     {
         "id": "golden-nanab",
         "name": "Golden Nanab",
-        "hungerRestored": 23,
+        "hungerRestored": 6,
         "cost": 15,
         "rarity": "Common",
         "file": "golden-nanab.png"
@@ -10,7 +10,7 @@ const pokemonBerries = [
     {
         "id": "chesto",
         "name": "Chesto",
-        "hungerRestored": 25,
+        "hungerRestored": 4,
         "cost": 18,
         "rarity": "Common",
         "file": "chesto.png"
@@ -18,7 +18,7 @@ const pokemonBerries = [
     {
         "id": "watmel",
         "name": "Watmel",
-        "hungerRestored": 28,
+        "hungerRestored": 7,
         "cost": 25,
         "rarity": "Common",
         "file": "watmel.png"
@@ -26,7 +26,7 @@ const pokemonBerries = [
     {
         "id": "persim",
         "name": "Persim",
-        "hungerRestored": 35,
+        "hungerRestored": 5,
         "cost": 39,
         "rarity": "Common",
         "file": "persim.png"
@@ -34,7 +34,7 @@ const pokemonBerries = [
     {
         "id": "golden-pinap",
         "name": "Golden Pinap",
-        "hungerRestored": 30,
+        "hungerRestored": 8,
         "cost": 26,
         "rarity": "Common",
         "file": "golden-pinap.png"
@@ -42,7 +42,7 @@ const pokemonBerries = [
     {
         "id": "oran",
         "name": "Oran",
-        "hungerRestored": 20,
+        "hungerRestored": 4,
         "cost": 10,
         "rarity": "Common",
         "file": "oran.png"
@@ -50,7 +50,7 @@ const pokemonBerries = [
     {
         "id": "rindo",
         "name": "Rindo",
-        "hungerRestored": 23,
+        "hungerRestored": 7,
         "cost": 16,
         "rarity": "Common",
         "file": "rindo.png"
@@ -58,7 +58,7 @@ const pokemonBerries = [
     {
         "id": "charti",
         "name": "Charti",
-        "hungerRestored": 32,
+        "hungerRestored": 5,
         "cost": 28,
         "rarity": "Common",
         "file": "charti.png"
@@ -66,7 +66,7 @@ const pokemonBerries = [
     {
         "id": "sitrus",
         "name": "Sitrus",
-        "hungerRestored": 35,
+        "hungerRestored": 8,
         "cost": 38,
         "rarity": "Common",
         "file": "sitrus.png"
@@ -74,7 +74,7 @@ const pokemonBerries = [
     {
         "id": "jaboca",
         "name": "Jaboca",
-        "hungerRestored": 22,
+        "hungerRestored": 6,
         "cost": 11,
         "rarity": "Common",
         "file": "jaboca.png"
@@ -82,7 +82,7 @@ const pokemonBerries = [
     {
         "id": "nanab",
         "name": "Nanab",
-        "hungerRestored": 26,
+        "hungerRestored": 4,
         "cost": 21,
         "rarity": "Common",
         "file": "nanab.png"
@@ -90,7 +90,7 @@ const pokemonBerries = [
     {
         "id": "nomel",
         "name": "Nomel",
-        "hungerRestored": 32,
+        "hungerRestored": 5,
         "cost": 29,
         "rarity": "Common",
         "file": "nomel.png"
@@ -98,7 +98,7 @@ const pokemonBerries = [
     {
         "id": "cheri",
         "name": "Cheri",
-        "hungerRestored": 24,
+        "hungerRestored": 7,
         "cost": 16,
         "rarity": "Common",
         "file": "cheri.png"
@@ -106,7 +106,7 @@ const pokemonBerries = [
     {
         "id": "qualot",
         "name": "Qualot",
-        "hungerRestored": 29,
+        "hungerRestored": 8,
         "cost": 25,
         "rarity": "Common",
         "file": "qualot.png"
@@ -114,7 +114,7 @@ const pokemonBerries = [
     {
         "id": "nanab-gen7",
         "name": "Nanab Gen7",
-        "hungerRestored": 31,
+        "hungerRestored": 5,
         "cost": 26,
         "rarity": "Common",
         "file": "nanab-gen7.png"
@@ -122,7 +122,7 @@ const pokemonBerries = [
     {
         "id": "payapa",
         "name": "Payapa",
-        "hungerRestored": 30,
+        "hungerRestored": 6,
         "cost": 25,
         "rarity": "Common",
         "file": "payapa.png"
@@ -130,7 +130,7 @@ const pokemonBerries = [
     {
         "id": "kebia",
         "name": "Kebia",
-        "hungerRestored": 26,
+        "hungerRestored": 4,
         "cost": 21,
         "rarity": "Common",
         "file": "kebia.png"
@@ -138,7 +138,7 @@ const pokemonBerries = [
     {
         "id": "apicot",
         "name": "Apicot",
-        "hungerRestored": 34,
+        "hungerRestored": 8,
         "cost": 34,
         "rarity": "Common",
         "file": "apicot.png"
@@ -146,7 +146,7 @@ const pokemonBerries = [
     {
         "id": "silver-nanab",
         "name": "Silver Nanab",
-        "hungerRestored": 33,
+        "hungerRestored": 7,
         "cost": 30,
         "rarity": "Common",
         "file": "silver-nanab.png"
@@ -154,7 +154,7 @@ const pokemonBerries = [
     {
         "id": "coba",
         "name": "Coba",
-        "hungerRestored": 24,
+        "hungerRestored": 6,
         "cost": 17,
         "rarity": "Common",
         "file": "coba.png"
@@ -162,7 +162,7 @@ const pokemonBerries = [
     {
         "id": "bluk",
         "name": "Bluk",
-        "hungerRestored": 34,
+        "hungerRestored": 5,
         "cost": 33,
         "rarity": "Common",
         "file": "bluk.png"
@@ -170,7 +170,7 @@ const pokemonBerries = [
     {
         "id": "aguav",
         "name": "Aguav",
-        "hungerRestored": 28,
+        "hungerRestored": 8,
         "cost": 24,
         "rarity": "Common",
         "file": "aguav.png"
@@ -178,7 +178,7 @@ const pokemonBerries = [
     {
         "id": "lum",
         "name": "Lum",
-        "hungerRestored": 35,
+        "hungerRestored": 4,
         "cost": 35,
         "rarity": "Common",
         "file": "lum.png"
@@ -186,7 +186,7 @@ const pokemonBerries = [
     {
         "id": "tanga",
         "name": "Tanga",
-        "hungerRestored": 21,
+        "hungerRestored": 7,
         "cost": 11,
         "rarity": "Common",
         "file": "tanga.png"
@@ -194,7 +194,7 @@ const pokemonBerries = [
     {
         "id": "chilan",
         "name": "Chilan",
-        "hungerRestored": 34,
+        "hungerRestored": 6,
         "cost": 34,
         "rarity": "Common",
         "file": "chilan.png"
@@ -202,7 +202,7 @@ const pokemonBerries = [
     {
         "id": "belue",
         "name": "Belue",
-        "hungerRestored": 35,
+        "hungerRestored": 5,
         "cost": 37,
         "rarity": "Common",
         "file": "belue.png"
@@ -210,7 +210,7 @@ const pokemonBerries = [
     {
         "id": "colbur",
         "name": "Colbur",
-        "hungerRestored": 20,
+        "hungerRestored": 4,
         "cost": 10,
         "rarity": "Common",
         "file": "colbur.png"
@@ -218,7 +218,7 @@ const pokemonBerries = [
     {
         "id": "starf",
         "name": "Starf",
-        "hungerRestored": 29,
+        "hungerRestored": 8,
         "cost": 25,
         "rarity": "Common",
         "file": "starf.png"
@@ -226,7 +226,7 @@ const pokemonBerries = [
     {
         "id": "golden-razz",
         "name": "Golden Razz",
-        "hungerRestored": 35,
+        "hungerRestored": 7,
         "cost": 38,
         "rarity": "Common",
         "file": "golden-razz.png"
@@ -234,7 +234,7 @@ const pokemonBerries = [
     {
         "id": "lansat",
         "name": "Lansat",
-        "hungerRestored": 21,
+        "hungerRestored": 5,
         "cost": 10,
         "rarity": "Common",
         "file": "lansat.png"
@@ -242,7 +242,7 @@ const pokemonBerries = [
     {
         "id": "kelpsy",
         "name": "Kelpsy",
-        "hungerRestored": 35,
+        "hungerRestored": 6,
         "cost": 38,
         "rarity": "Common",
         "file": "kelpsy.png"
@@ -250,7 +250,7 @@ const pokemonBerries = [
     {
         "id": "custap",
         "name": "Custap",
-        "hungerRestored": 31,
+        "hungerRestored": 8,
         "cost": 27,
         "rarity": "Common",
         "file": "custap.png"
@@ -258,7 +258,7 @@ const pokemonBerries = [
     {
         "id": "pamtre",
         "name": "Pamtre",
-        "hungerRestored": 35,
+        "hungerRestored": 4,
         "cost": 36,
         "rarity": "Common",
         "file": "pamtre.png"
@@ -266,7 +266,7 @@ const pokemonBerries = [
     {
         "id": "shuca",
         "name": "Shuca",
-        "hungerRestored": 33,
+        "hungerRestored": 7,
         "cost": 33,
         "rarity": "Common",
         "file": "shuca.png"
@@ -274,7 +274,7 @@ const pokemonBerries = [
     {
         "id": "pinap",
         "name": "Pinap",
-        "hungerRestored": 27,
+        "hungerRestored": 5,
         "cost": 22,
         "rarity": "Common",
         "file": "pinap.png"
@@ -282,7 +282,7 @@ const pokemonBerries = [
     {
         "id": "rowap",
         "name": "Rowap",
-        "hungerRestored": 27,
+        "hungerRestored": 6,
         "cost": 23,
         "rarity": "Common",
         "file": "rowap.png"
@@ -290,7 +290,7 @@ const pokemonBerries = [
     {
         "id": "babiri",
         "name": "Babiri",
-        "hungerRestored": 35,
+        "hungerRestored": 8,
         "cost": 36,
         "rarity": "Common",
         "file": "babiri.png"
@@ -298,7 +298,7 @@ const pokemonBerries = [
     {
         "id": "maranga",
         "name": "Maranga",
-        "hungerRestored": 35,
+        "hungerRestored": 4,
         "cost": 37,
         "rarity": "Common",
         "file": "maranga.png"
@@ -306,7 +306,7 @@ const pokemonBerries = [
     {
         "id": "durin",
         "name": "Durin",
-        "hungerRestored": 25,
+        "hungerRestored": 5,
         "cost": 18,
         "rarity": "Common",
         "file": "durin.png"
@@ -314,7 +314,7 @@ const pokemonBerries = [
     {
         "id": "magost",
         "name": "Magost",
-        "hungerRestored": 22,
+        "hungerRestored": 7,
         "cost": 14,
         "rarity": "Common",
         "file": "magost.png"
@@ -322,7 +322,7 @@ const pokemonBerries = [
     {
         "id": "iapapa",
         "name": "Iapapa",
-        "hungerRestored": 60,
+        "hungerRestored": 11,
         "cost": 100,
         "rarity": "Rare",
         "file": "iapapa.png"
@@ -330,7 +330,7 @@ const pokemonBerries = [
     {
         "id": "ganlon",
         "name": "Ganlon",
-        "hungerRestored": 59,
+        "hungerRestored": 14,
         "cost": 97,
         "rarity": "Rare",
         "file": "ganlon.png"
@@ -338,7 +338,7 @@ const pokemonBerries = [
     {
         "id": "tamato",
         "name": "Tamato",
-        "hungerRestored": 58,
+        "hungerRestored": 10,
         "cost": 96,
         "rarity": "Rare",
         "file": "tamato.png"
@@ -346,7 +346,7 @@ const pokemonBerries = [
     {
         "id": "rabuta",
         "name": "Rabuta",
-        "hungerRestored": 56,
+        "hungerRestored": 12,
         "cost": 92,
         "rarity": "Rare",
         "file": "rabuta.png"
@@ -354,7 +354,7 @@ const pokemonBerries = [
     {
         "id": "liechi",
         "name": "Liechi",
-        "hungerRestored": 40,
+        "hungerRestored": 13,
         "cost": 51,
         "rarity": "Rare",
         "file": "liechi.png"
@@ -362,7 +362,7 @@ const pokemonBerries = [
     {
         "id": "passho",
         "name": "Passho",
-        "hungerRestored": 44,
+        "hungerRestored": 11,
         "cost": 69,
         "rarity": "Rare",
         "file": "passho.png"
@@ -370,7 +370,7 @@ const pokemonBerries = [
     {
         "id": "roseli",
         "name": "Roseli",
-        "hungerRestored": 58,
+        "hungerRestored": 10,
         "cost": 93,
         "rarity": "Rare",
         "file": "roseli.png"
@@ -378,7 +378,7 @@ const pokemonBerries = [
     {
         "id": "silver-pinap",
         "name": "Silver Pinap",
-        "hungerRestored": 55,
+        "hungerRestored": 14,
         "cost": 86,
         "rarity": "Rare",
         "file": "silver-pinap.png"
@@ -386,7 +386,7 @@ const pokemonBerries = [
     {
         "id": "figy",
         "name": "Figy",
-        "hungerRestored": 50,
+        "hungerRestored": 12,
         "cost": 77,
         "rarity": "Rare",
         "file": "figy.png"
@@ -394,7 +394,7 @@ const pokemonBerries = [
     {
         "id": "mago",
         "name": "Mago",
-        "hungerRestored": 52,
+        "hungerRestored": 13,
         "cost": 82,
         "rarity": "Rare",
         "file": "mago.png"
@@ -402,7 +402,7 @@ const pokemonBerries = [
     {
         "id": "hondew",
         "name": "Hondew",
-        "hungerRestored": 54,
+        "hungerRestored": 11,
         "cost": 86,
         "rarity": "Rare",
         "file": "hondew.png"
@@ -410,7 +410,7 @@ const pokemonBerries = [
     {
         "id": "grepa",
         "name": "Grepa",
-        "hungerRestored": 57,
+        "hungerRestored": 10,
         "cost": 92,
         "rarity": "Rare",
         "file": "grepa.png"
@@ -418,7 +418,7 @@ const pokemonBerries = [
     {
         "id": "salac",
         "name": "Salac",
-        "hungerRestored": 60,
+        "hungerRestored": 14,
         "cost": 115,
         "rarity": "Rare",
         "file": "salac.png"
@@ -426,7 +426,7 @@ const pokemonBerries = [
     {
         "id": "yache",
         "name": "Yache",
-        "hungerRestored": 41,
+        "hungerRestored": 12,
         "cost": 56,
         "rarity": "Rare",
         "file": "yache.png"
@@ -434,7 +434,7 @@ const pokemonBerries = [
     {
         "id": "wepear",
         "name": "Wepear",
-        "hungerRestored": 42,
+        "hungerRestored": 13,
         "cost": 62,
         "rarity": "Rare",
         "file": "wepear.png"
@@ -442,7 +442,7 @@ const pokemonBerries = [
     {
         "id": "petaya",
         "name": "Petaya",
-        "hungerRestored": 46,
+        "hungerRestored": 11,
         "cost": 72,
         "rarity": "Rare",
         "file": "petaya.png"
@@ -450,7 +450,7 @@ const pokemonBerries = [
     {
         "id": "occa",
         "name": "Occa",
-        "hungerRestored": 59,
+        "hungerRestored": 14,
         "cost": 96,
         "rarity": "Rare",
         "file": "occa.png"
@@ -458,7 +458,7 @@ const pokemonBerries = [
     {
         "id": "spelon",
         "name": "Spelon",
-        "hungerRestored": 51,
+        "hungerRestored": 10,
         "cost": 77,
         "rarity": "Rare",
         "file": "spelon.png"
@@ -466,7 +466,7 @@ const pokemonBerries = [
     {
         "id": "wacan",
         "name": "Wacan",
-        "hungerRestored": 53,
+        "hungerRestored": 12,
         "cost": 82,
         "rarity": "Rare",
         "file": "wacan.png"
@@ -474,7 +474,7 @@ const pokemonBerries = [
     {
         "id": "kasib",
         "name": "Kasib",
-        "hungerRestored": 60,
+        "hungerRestored": 13,
         "cost": 106,
         "rarity": "Rare",
         "file": "kasib.png"
@@ -482,7 +482,7 @@ const pokemonBerries = [
     {
         "id": "rawst",
         "name": "Rawst",
-        "hungerRestored": 45,
+        "hungerRestored": 11,
         "cost": 70,
         "rarity": "Rare",
         "file": "rawst.png"
@@ -490,7 +490,7 @@ const pokemonBerries = [
     {
         "id": "silver-razz",
         "name": "Silver Razz",
-        "hungerRestored": 60,
+        "hungerRestored": 14,
         "cost": 117,
         "rarity": "Rare",
         "file": "silver-razz.png"
@@ -498,7 +498,7 @@ const pokemonBerries = [
     {
         "id": "chople",
         "name": "Chople",
-        "hungerRestored": 48,
+        "hungerRestored": 10,
         "cost": 75,
         "rarity": "Rare",
         "file": "chople.png"
@@ -506,7 +506,7 @@ const pokemonBerries = [
     {
         "id": "razz-gen7",
         "name": "Razz Gen7",
-        "hungerRestored": 60,
+        "hungerRestored": 12,
         "cost": 107,
         "rarity": "Rare",
         "file": "razz-gen7.png"
@@ -514,7 +514,7 @@ const pokemonBerries = [
     {
         "id": "razz",
         "name": "Razz",
-        "hungerRestored": 49,
+        "hungerRestored": 13,
         "cost": 76,
         "rarity": "Rare",
         "file": "razz.png"
@@ -522,7 +522,7 @@ const pokemonBerries = [
     {
         "id": "cornn",
         "name": "Cornn",
-        "hungerRestored": 73,
+        "hungerRestored": 18,
         "cost": 181,
         "rarity": "Mythical",
         "file": "cornn.png"
@@ -530,7 +530,7 @@ const pokemonBerries = [
     {
         "id": "pecha",
         "name": "Pecha",
-        "hungerRestored": 75,
+        "hungerRestored": 15,
         "cost": 182,
         "rarity": "Mythical",
         "file": "pecha.png"
@@ -538,7 +538,7 @@ const pokemonBerries = [
     {
         "id": "aspear",
         "name": "Aspear",
-        "hungerRestored": 80,
+        "hungerRestored": 20,
         "cost": 215,
         "rarity": "Mythical",
         "file": "aspear.png"
@@ -546,7 +546,7 @@ const pokemonBerries = [
     {
         "id": "kee",
         "name": "Kee",
-        "hungerRestored": 65,
+        "hungerRestored": 16,
         "cost": 155,
         "rarity": "Mythical",
         "file": "kee.png"
@@ -554,7 +554,7 @@ const pokemonBerries = [
     {
         "id": "pomeg",
         "name": "Pomeg",
-        "hungerRestored": 92,
+        "hungerRestored": 21,
         "cost": 254,
         "rarity": "Mythical",
         "file": "pomeg.png"
@@ -562,7 +562,7 @@ const pokemonBerries = [
     {
         "id": "micle",
         "name": "Micle",
-        "hungerRestored": 85,
+        "hungerRestored": 17,
         "cost": 226,
         "rarity": "Mythical",
         "file": "micle.png"
@@ -570,7 +570,7 @@ const pokemonBerries = [
     {
         "id": "wiki",
         "name": "Wiki",
-        "hungerRestored": 98,
+        "hungerRestored": 19,
         "cost": 284,
         "rarity": "Mythical",
         "file": "wiki.png"
@@ -578,7 +578,7 @@ const pokemonBerries = [
     {
         "id": "enigma",
         "name": "Enigma",
-        "hungerRestored": 90,
+        "hungerRestored": 15,
         "cost": 249,
         "rarity": "Mythical",
         "file": "enigma.png"
@@ -586,7 +586,7 @@ const pokemonBerries = [
     {
         "id": "leppa",
         "name": "Leppa",
-        "hungerRestored": 100,
+        "hungerRestored": 21,
         "cost": 287,
         "rarity": "Mythical",
         "file": "leppa.png"
@@ -594,7 +594,7 @@ const pokemonBerries = [
     {
         "id": "haban",
         "name": "Haban",
-        "hungerRestored": 70,
+        "hungerRestored": 18,
         "cost": 174,
         "rarity": "Mythical",
         "file": "haban.png"
